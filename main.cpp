29896f7736f0cc5d8f7949f294284af6daa06560
Lis, Andrzej <andrzej.lis3@gmail.com>
2021-03-16 12:27:07 +0000

    make header only

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "rdg.h"
 
 int main() {
-    auto dungeon = rdg::Dungeon::create_dungeon(rdg::Options());
+    auto dungeon = rdg<>::create_dungeon(rdg<>::Options());
     for (const auto &row:dungeon.getCells()) {
         for (auto cell:row) {
             if (cell.hasLabel()) {
                 std::cout << cell.getLabel();
-            } else if (cell.hasType(rdg::ROOM)) {
+            } else if (cell.hasType(rdg<>::ROOM)) {
                 std::cout << "X";
-            } else if (cell.hasType(rdg::CORRIDOR)) {
+            } else if (cell.hasType(rdg<>::CORRIDOR)) {
                 std::cout << "x";
             } else if (cell.isDoorspace()) {
                 std::cout << "D";
